440b3cf83fba27be459b83982d05518b8ec455cd
NarraRineesh <62957541+NarraRineesh@users.noreply.github.com>
2021-07-02 19:11:29 +0530

    # route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ children: [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,  {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyDPoxnwK4CgrlblH8dr2gtvNpXhDPo6vF0",
+    authDomain: "bloodapp-9e3cb.firebaseapp.com",
+    projectId: "bloodapp-9e3cb",
+    storageBucket: "bloodapp-9e3cb.appspot.com",
+    messagingSenderId: "139624470875",
+    appId: "1:139624470875:web:6365365caabea07ad7227e"
+  }
 };
